0c2bab0d65131930a5a10b604cfbcf54fdd2ed8c
Ramazan01 <ramazan.ayazbek.kz@gmail.com>
2023-05-21 12:53:15 +0600

    movie_website1

---
+++ movies/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Review, Rating, RatingStar
+# from snowpenguin.django.recaptcha3.fields import ReCaptchaField
+# from snowpenguin.django.recaptcha3.fields import ReCaptchaField
+# from captcha.fields import ReCaptchaField
+
+class ReviewForm(forms.ModelForm):
+    # captcha=ReCaptchaField()
+    class Meta:
+        model=Review
+        fields=("name", "email", "text", "captcha")
+        widgets={
+            "name":forms.TextInput(attrs={"class":"form-control border"}),
+            "email":forms.EmailInput(attrs={"class":"form-control border"}),
+            "text":forms.Textarea(attrs={"class":"form-control border"})
+        }
+
+
+class RatingForm(forms.ModelForm):
+    star=forms.ModelChoiceField(
+        queryset=RatingStar.objects.all(), widget=forms.RadioSelect(), empty_label=None
+        )
+
+    class Meta:
+        model=Rating
+        fields=('star',)
+
---
+++ movies/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import MoviesView, MovieDetialView, AddReview, ActorView, FilterMoviesView, AddStarRating, Search
+urlpatterns = [
+    path('', MoviesView.as_view()),
+    path("filter/", FilterMoviesView.as_view(), name="filter"),
+    path("search/", Search.as_view(), name="search"),
+    path("add-rating/", AddStarRating.as_view(), name='add_rating'),
+    path('<slug:slug>/', MovieDetialView.as_view(), name='movie_detail'),
+    path('review/<int:pk>/', AddReview.as_view(), name='add_review'),
+    path('actor/<str:slug>/', ActorView.as_view(), name='actor_detail')
+]
